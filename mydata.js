c4f5f87c884772a0c1ab1218b9aeb4a27e818525
Christian <s535636@nwmissouri.edu>
2023-03-29 11:28:41 -0500

    bunch of stuff

---
+++ mydata.js
@@ -0,0 +1,11 @@
+<label id='Christian'>Christian Keller</label>
+
+document.getElementById('Christian').setAttribute('title');
+
+
+
+fetch(‘http://some.place.com/service’)
+
+mydata = require('./routes/mydata');
+
+app.use('/mydata',indexRouter);
